22353552dd3059a4540118d2263d5e0c25e78105
lucas2350d <69628182+lucas2350d@users.noreply.github.com>
2022-09-28 12:43:33 +0200

    Update recommendation.py

--- app/recommendation.py
+++ app/recommendation.py
@@ -1,6 +1,9 @@
 listOfMovies = []
 listOfRecommendations = []
 
+@app.route("/")
+def hello_world():
+    return "<p>Go to /movies for a List of Movies</p>"
 
 def getListOfMovies():         
         listOfMovies = ["Film1", "Film2", "Film3", "Film4", "Film5"]
