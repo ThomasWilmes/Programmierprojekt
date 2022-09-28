f6ca290a19e050f11fc6e8da21e8ec0f2438639a
lucas2350d <69628182+lucas2350d@users.noreply.github.com>
2022-09-28 09:55:08 +0200

    Create recommendation.py

---
+++ app/recommendation.py
@@ -0,0 +1,15 @@
+listOfMovies = []
+listOfRecommendations = []
+
+
+def getListOfMovies():         
+        listOfMovies = ["Film1", "Film2", "Film3", "Film4", "Film5"]
+        return listOfMovies
+
+
+def getListOfRecommendations(movies):
+    return movies
+
+
+def getRecommendation():
+    return None
