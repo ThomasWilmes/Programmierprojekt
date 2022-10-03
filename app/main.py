b0b0a47ab1f034a5a0724e1861a1c985aafdc7a1
lucas2350d <69628182+lucas2350d@users.noreply.github.com>
2022-10-03 14:24:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,8 +6,10 @@ import os
 from flask import Flask, request
 from flask_restful import Api
 from recommendation import *
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 def options (self):
