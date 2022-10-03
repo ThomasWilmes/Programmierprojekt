9adbf36641854af502880a7a7b116b0f11350067
lucas2350d <69628182+lucas2350d@users.noreply.github.com>
2022-10-03 16:32:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,26 +12,9 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-def options (self):
-    return {'Allow' : 'PUT' }, 200, \
-    { 'Access-Control-Allow-Origin': '*', \
-      'Access-Control-Allow-Methods' : 'PUT,GET' }
-
 @app.route('/movies')
-def get_movies():
-    return getListOfMovies()
-
-
-@app.route('/recommendation')
-def get_recommendation():
-    requestData = request.form.get('movies')
-    if (requestData is None):
-        return 'No input data was given', 400
-    movieList = requestData.split(',')
-    if (movieList is None):
-        return 'The input data is not in the correct formatt', 400
-    return getListOfRecommendations(movieList)
-
+def get_movielist():
+    return getMovieList()
 
 if __name__=='__main__':
     cfg_port = os.getenv('PORT', "5000")
