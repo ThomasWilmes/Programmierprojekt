2b4ce773987fe06f70372bb1c989fb4140e8b4d0
lucas2350d <69628182+lucas2350d@users.noreply.github.com>
2022-10-03 16:32:40 +0200

    Update recommendation.py

--- app/recommendation.py
+++ app/recommendation.py
@@ -8,8 +8,9 @@ listOfRecommendations = []
 
 @dataclass(unsafe_hash=True)
 class Movie:
-    id: int
     title: str
+    id: int
+
 
 #def parseCSV(filePath):
       # CVS Column Names
@@ -20,14 +21,14 @@ class Movie:
 #      for i,row in csvData.iterrows():
 #            return (i,row['id'],row['film'],)
 
-def getListOfMovies():
-    global listOfMovies
+def getMovieList():
+    global movieList
     with open("data/movie_titles.csv", encoding='latin-1') as f:
         for line in f:
-            listOfMovies.append(create_movie(line))
-        return listOfMovies
+            movieList.append(add_movie(line))
+        return movieList
 
-def create_movie(line):
+def add_movie(line):
     movie = line.split(',')
     movie_id = movie[0]
     movie_title = movie[1]
@@ -35,10 +36,3 @@ def create_movie(line):
     if (movie.count('') < len(movie)):
         movie_title += ','.join(movie)
     return Movie(id=movie_id, title=movie_title)
-
-def getListOfRecommendations(movies):
-    return movies
-
-
-def getRecommendation():
-    return None
