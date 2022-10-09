c6a29b71dc323e4d0bb65b739f6c37996f1d5fa3
lucas2350d <69628182+lucas2350d@users.noreply.github.com>
2022-10-09 14:42:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ CORS(app)
 api = Api(app)
 
 
-@app.route('/movies')
+@app.route('/movies', methods=['GET'])
 def get_list():
     """Return the Full Movie List"""
     return get_movie_list()
@@ -24,6 +24,22 @@ def with_parameters():
     film_name = request.args.get('filmname')
     film_id = int(request.args.get('filmid'))
     return jsonify(message="The Film: " + str(film_name) + " has the ID:" + film_id)
+
+@app.route('/recommendation', methods=['GET'])
+def get_recommendation():
+    """
+    Recommendations Endpoint
+    """
+    request_data = request.args.get('movies_ids')
+    if (request_data is None):
+        return 'No input data was given', 400
+    try:
+        if ',' in request_data:
+            movie_id = [int(movie) for movie in request_data.split(',')]
+        else:
+            movie_id = [int(request_data)]
+        return get_movie_list(movie_id)
+   
 
 if __name__=='__main__':
     cfg_port = os.getenv('PORT', "5000")
