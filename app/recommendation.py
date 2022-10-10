f7401c22a576c20003c8ec6d9b3b775479998fd0
Lucas2350d <lucas.grewe@icloud.com>
2022-10-10 17:27:41 +0200

    test

--- app/recommendation.py
+++ app/recommendation.py
@@ -12,7 +12,7 @@ class Movie:
 
 class Recommendation:
     movie_id: int
-    recommendations: str
+    recommendations: list[int]
 
 
 def get_movie_list() -> list[Movie]:
@@ -22,28 +22,8 @@ def get_movie_list() -> list[Movie]:
         for eachLine in f:
              movieList.append(add_movie(eachLine))
         return  movieList
-
-
-def get_recommendation_list() -> list[Recommendation]:
-    '''Return a List of all Movies'''
-    movieList: list[Recommendation] = []
-    with open("data/Movie_Recommendations_comma.csv", encoding='latin-1') as f:
-        for eachLine in f:
-             movieList.append(add_recommendation(eachLine))
-        return  movieList
    
 
-def add_recommendation(line):
-    '''add a recommendation to the List'''
-    movie = line.split(',')
-    movie[-1] = movie[-1].replace('\n', '')
-    movie_id = movie.pop(0)
-    if (len(movie) > 1):
-        movie_title = movie.pop(0)
-        movie_title += ','.join(movie)
-    else:
-        movie_title = movie[0]
-    return Recommendation(movie_id=movie_id, recommendations=movie_title)
 
 def add_movie(line):
     '''add a Movie to the List'''
