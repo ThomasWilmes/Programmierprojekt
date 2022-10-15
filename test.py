6755f8b570f75d2d3c0af046056ad9b976f07669
magicVeve <86608049+magicVeve@users.noreply.github.com>
2022-10-15 12:12:17 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+import unittest
+from app.recommandation import get_list_of_recommendation
+
+__all__ = ['get_list_of_movies', 'Movie']
+
+
+class TestApiFunctionallity(unittest.TestCase):
+    '''
+    API Unit tests
+    '''
+
+    def test_RecSystemOutputFiveMovies(self) -> None:
+        '''
+        tests if the right length of recommended movies is given
+        '''     
+        id = {3}
+        recommandedMovies = get_list_of_recommendation(id)
+
+        self.assertEqual(5, len(recommandedMovies["recommendation"]))
+
+    def test_RecSystemOutputZeroWhenNoID(self) -> None:
+        '''
+        tests if recommended movies empty when there is no id
+        '''     
+        id = {}
+        recommandedMovies = get_list_of_recommendation(id)
+
+        self.assertEqual(0, len(recommandedMovies["recommendation"]))
+
+    def test_RecSystemOutputWithMultipleIDs(self) -> None:
+        '''
+        tests if recommended movies empty when there is no valid id
+        '''     
+        id = {8,3}
+        recommandedMovies = get_list_of_recommendation(id)
+
+        self.assertEqual(5, len(recommandedMovies["recommendation"]))
+
+
+if __name__ == '__main__':
+    '''
+    enables execution of the tests as a seperate file for ci/cd
+    '''
+    unittest.main()
